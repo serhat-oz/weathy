fa6d0d8cf92e6b1940b2bfd77af4ee3a38cb67f6
serhat-oz <serhatozturk@outlook.com>
2020-10-11 12:32:26 +0300

    base variables created in Constants.kt class, api key created from accuweather.com/

---
+++ app/src/main/java/com/serhat/weather/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.serhat.weather.util
+
+class Constants {
+    companion object{
+        const val BASE_URL = "http://dataservice.accuweather.com/"
+        const val API_KEY = "QxI3Igk1E6pwmFKH862DsP47NpaamJGU"
+        const val REQUEST_TIMEOUT_DURATION = 30
+    }
+}
